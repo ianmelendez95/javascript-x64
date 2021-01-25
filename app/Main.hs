b0c54de26aaa29795da7a8a97517a547a981cd04
Ian Melendez <imelendez@preludedynamics.com>
2021-01-25 11:55:55 -0600

    added arg parsing example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
-import Lib
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
-main = putStrLn "Hello world!"
+main = greet =<< execParser opts
+  where
+    opts = info (helper <*> sample)
+      ( fullDesc
+        <> progDesc "Print a greeting for TARGET"
+        <> header "hello - a test for optparse-applicative" )
+
+greet :: Sample -> IO ()
+greet (Sample h False n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+greet _ = return ()
+
+data Sample = Sample
+  { hello      :: String
+  , quiet      :: Bool
+  , enthusiasm :: Int }
+
+sample :: Parser Sample
+sample = Sample
+      <$> strOption
+          ( long "hello"
+            <> metavar "TARGET"
+            <> help "Target for the greeting" )
+      <*> switch
+          ( long "quiet"
+            <> short 'q'
+            <> help "Whether to be quiet" )
+      <*> option auto
+          ( long "enthusiasm"
+            <> help "How enthusiastically to greet"
+            <> showDefault
+            <> value 1
+            <> metavar "INT" )
