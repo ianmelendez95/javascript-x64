d294ee997b21eae59b3d1dafb077221bddce9e0c
Ian Melendez <imelendez@preludedynamics.com>
2021-01-29 15:51:26 -0600

    repl now maintains environment

--- app/Main.hs
+++ app/Main.hs
@@ -17,27 +17,27 @@ import JS.Value
 import JS.Runtime
 
 main :: IO ()
-main = runInputT defaultSettings loop
-  where
-    loop :: InputT IO ()
-    loop = do
-      minput <- getInputLine "> "
-      case minput of
-        Nothing -> return ()
-        Just ".exit" -> return ()
-        Just input -> 
-          case parseExpr input of 
-            (Left err) -> outputStrLn (errorBundlePretty err) >> loop
-            (Right parsed) -> 
-              do outputStrLn $ "Parsed: " ++ show parsed
-                 evalResult <- liftIO $ evalStart parsed
-                 case evalResult of 
-                   (Left err) -> outputStrLn ("JS Error: " ++ show err) >> loop
-                   (Right res) -> outputStrLn (show res) >> loop
+main = runInputT defaultSettings (loop emptyEnvironment)
 
+loop :: Environment -> InputT IO ()
+loop env = 
+  do minput <- getInputLine "> "
+     case minput of
+       Nothing -> return ()
+       Just ".exit" -> return ()
+       Just input -> 
+         case parseExpr input of 
+           (Left err) -> outputStrLn (errorBundlePretty err) >> loop env
+           (Right parsed) -> 
+             do outputStrLn $ "Parsed: " ++ show parsed
+                evalResult <- liftIO $ evalStart parsed
+                case evalResult of 
+                  (Left err) -> outputStrLn ("JS Error: " ++ show err) >> loop env
+                  (Right (res, env')) -> outputStrLn (show res) >> loop env'
+  where 
     printParsed :: Either ParseError Expr -> InputT IO ()
     printParsed (Left err) = outputStrLn $ errorBundlePretty err
     printParsed (Right expr) = outputStrLn $ show expr
 
-    evalStart :: Expr -> IO (Either JSError Value)
-    evalStart expr = runExceptT . evalStateT (eval expr) $ emptyEnvironment
+    evalStart :: Expr -> IO (Either JSError (Value, Environment))
+    evalStart expr = runExceptT . runStateT (eval expr) $ env
