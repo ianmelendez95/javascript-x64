1136b8b97a11c69cd82f22e54aed7af50e3427ff
Ian Melendez <imelendez@preludedynamics.com>
2021-02-02 21:12:29 -0600

    added parsing acorn output to json value

--- src/Acorn/Client.hs
+++ src/Acorn/Client.hs
@@ -6,9 +6,16 @@ import System.Process
       CreateProcess(std_in, std_out),
       StdStream(CreatePipe) )
 import GHC.IO.Handle ( hGetContents, hPutStr, hClose )
+import Data.Aeson (Value (..), eitherDecode)
+import qualified Data.ByteString.Lazy as B
+import Data.Either
 
-invokeAcornClient :: String -> IO String
-invokeAcornClient js_source = do create_result <- createProcess (proc "node" ["acorn-client/acorn-client.js"]){ std_in = CreatePipe, std_out = CreatePipe }
-                                 case create_result of 
-                                   (Just stdin, Just stdout, _, _) -> hPutStr stdin js_source >> hClose stdin >> hGetContents stdout
-                                   _ -> error "Expecting stdin and stdout handles"
+invokeAcornClientJson :: String -> IO Value
+invokeAcornClientJson js_source = either error id . eitherDecode <$> invokeAcornClientRaw js_source
+
+invokeAcornClientRaw :: String -> IO B.ByteString 
+invokeAcornClientRaw js_source = do create_result <- createProcess (proc "node" ["acorn-client/acorn-client.js"]){ std_in = CreatePipe, std_out = CreatePipe }
+                                    case create_result of 
+                                      (Just stdin, Just stdout, _, _) -> hPutStr stdin js_source >> hClose stdin >> B.hGetContents stdout
+                                      _ -> error "Expecting stdin and stdout handles"
+
